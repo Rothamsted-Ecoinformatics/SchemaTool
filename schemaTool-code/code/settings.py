a2f5599787f627631d9c415e5f6ebe06e5d2199e
Nathalie Castells <nathalie.castells@rothamsted.ac.uk>
2019-11-20 11:53:11 +0000

    updates stations

--- schemaTool-code/code/settings.py
+++ schemaTool-code/code/settings.py
@@ -8,4 +8,4 @@ Created on 13 june 2019
 ROOT = "http://local-info.rothamsted.ac.uk/eRA/era2018-new/"
 STAGE = "d:/eRAstage6/"
 DEFAULT = "metadata/default/"
-STATIONS = ["default", "rothamsted", "broom", "woburn", "saxmundham"] # list of extra directories needed in metadata folders. 
+STATIONS = ["default", "rothamsted", "broom", "woburn", "saxmundham", "met", "rms", "bms", "wms", "sms", "rro", "rrn2"] # list of extra directories needed in metadata folders. 
